lichti1901/android_packages_apps_settings

Settings: some fixes and addaptions to LP for PGM
- add a reset icon - backup icon has changed which we used in the past
- fix appops details call
- reorder PGM position.

src/com/android/settings/SecuritySettings.java
@@ -196,12 +196,17 @@ public class SecuritySettings extends SettingsPreferenceFragment
         addPreferencesFromResource(R.xml.security_settings);
         root = getPreferenceScreen();
 
+        // Add options for device encryption
+        mIsPrimary = UserHandle.myUserId() == UserHandle.USER_OWNER;
+
+        if (mIsPrimary) {
+            // App security settings
+            addPreferencesFromResource(R.xml.system_settings);
+        }
+
         // Add options for lock/unlock screen
         final int resid = getResIdForLockUnlockScreen(getActivity(), mLockPatternUtils);
         addPreferencesFromResource(resid);
-
-        // Add options for device encryption
-        mIsPrimary = UserHandle.myUserId() == UserHandle.USER_OWNER;
 
         if (!mIsPrimary) {
             // Rename owner info settings
@@ -344,11 +349,6 @@ public class SecuritySettings extends SettingsPreferenceFragment
         if (um.hasUserRestriction(UserManager.DISALLOW_INSTALL_UNKNOWN_SOURCES)
                 || um.hasUserRestriction(UserManager.DISALLOW_INSTALL_APPS)) {
             mToggleAppInstallation.setEnabled(false);
-        }
-
-        if (mIsPrimary) {
-            // App security settings
-            addPreferencesFromResource(R.xml.system_settings);
         }
 
         // Advanced Security features

Hybrid-Rom/packages_apps_Settings

Change-Id: I8de6da483651bc83a5f711a07633e1c3393a1611
Conflicts:
	src/com/android/settings/SecuritySettings.java
	src/com/android/settings/applications/AppOpsDetails.java
	src/com/android/settings/slim/privacyguard/PrivacyGuardManager.java

src/com/android/settings/SecuritySettings.java
@@ -203,12 +203,17 @@ public class SecuritySettings extends SettingsPreferenceFragment
         addPreferencesFromResource(R.xml.security_settings);
         root = getPreferenceScreen();
 
+        // Add options for device encryption
+        mIsPrimary = UserHandle.myUserId() == UserHandle.USER_OWNER;
+
+        if (mIsPrimary) {
+            // App security settings
+            addPreferencesFromResource(R.xml.security_settings_app_slim);
+        }
+
         // Add options for lock/unlock screen
         final int resid = getResIdForLockUnlockScreen(getActivity(), mLockPatternUtils);
         addPreferencesFromResource(resid);
-
-        // Add options for device encryption
-        mIsPrimary = UserHandle.myUserId() == UserHandle.USER_OWNER;
 
         if (!mIsPrimary) {
             // Rename owner info settings
@@ -386,14 +391,6 @@ public class SecuritySettings extends SettingsPreferenceFragment
         if (um.hasUserRestriction(UserManager.DISALLOW_INSTALL_UNKNOWN_SOURCES)
                 || um.hasUserRestriction(UserManager.DISALLOW_INSTALL_APPS)) {
             mToggleAppInstallation.setEnabled(false);
-        }
-
-        // AppOps summary, only visible when strict mode is enabled.
-        if (!AppOpsManager.isStrictEnable()) {
-            Preference appOpsSummary = findPreference(KEY_APP_OPS_SUMMARY);
-            if (deviceAdminCategory != null) {
-                deviceAdminCategory.removePreference(appOpsSummary);
-            }
         }
 
         // Advanced Security features